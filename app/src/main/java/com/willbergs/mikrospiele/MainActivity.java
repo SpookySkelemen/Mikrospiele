96a00d941cde65749d845b418b37c05abc4fa241
Willburger <michael.willburger071@sccdsb.net>
2017-06-06 09:55:56 -0400

    Initial commit

---
+++ app/src/main/java/com/willbergs/mikrospiele/MainActivity.java
@@ -0,0 +1,27 @@
+package com.willbergs.mikrospiele;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void toHelpScreen (View view){
+        Intent helpIntent = new Intent (this, HelpActivity.class);
+        startActivity(helpIntent);
+    }
+
+
+    public void toBegin (View view){
+        Intent transitionIntent = new Intent (this, TransitionActivity.class);
+        startActivity(transitionIntent);
+    }
+}
