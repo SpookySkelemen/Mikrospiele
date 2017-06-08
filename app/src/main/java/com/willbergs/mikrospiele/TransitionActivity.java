59f0c8380f1bf6063561a4ea98b7186d2e60733b
michael.willburger07 <generalflapjack8@gmail.com>
2017-06-08 10:09:39 -0400

    Added transition screen animation

--- app/src/main/java/com/willbergs/mikrospiele/TransitionActivity.java
+++ app/src/main/java/com/willbergs/mikrospiele/TransitionActivity.java
@@ -2,6 +2,12 @@ package com.willbergs.mikrospiele;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import static com.willbergs.mikrospiele.R.id.imgHeart1;
+import static com.willbergs.mikrospiele.R.id.imgHeart2;
+import static com.willbergs.mikrospiele.R.id.imgHeart3;
+import static java.lang.Thread.sleep;
 
 public class TransitionActivity extends Activity {
 
@@ -11,5 +17,50 @@ public class TransitionActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transition);
+        transition();
+    }
+
+
+    public void transition (){
+        ImageView heart1 = (ImageView) findViewById(imgHeart1);
+        ImageView heart2 = (ImageView) findViewById(imgHeart2);
+        ImageView heart3 = (ImageView) findViewById(imgHeart3);
+
+        for (int x = 0 ; x < 4 ; x++){
+            try {
+                heart1.setImageResource(R.drawable.heart_1);
+                heart2.setImageResource(R.drawable.heart_1);
+                heart3.setImageResource(R.drawable.heart_1);
+                sleep(33);
+                heart1.setImageResource(R.drawable.heart_2);
+                heart2.setImageResource(R.drawable.heart_2);
+                heart3.setImageResource(R.drawable.heart_2);
+                sleep(33);
+                heart1.setImageResource(R.drawable.heart_3);
+                heart2.setImageResource(R.drawable.heart_3);
+                heart3.setImageResource(R.drawable.heart_3);
+                sleep(33);
+                heart1.setImageResource(R.drawable.heart_4);
+                heart2.setImageResource(R.drawable.heart_4);
+                heart3.setImageResource(R.drawable.heart_4);
+                sleep(33);
+                heart1.setImageResource(R.drawable.heart_5);
+                heart2.setImageResource(R.drawable.heart_5);
+                heart3.setImageResource(R.drawable.heart_5);
+                sleep(33);
+                heart1.setImageResource(R.drawable.heart_6);
+                heart2.setImageResource(R.drawable.heart_6);
+                heart3.setImageResource(R.drawable.heart_6);
+                sleep(33);
+                heart1.setImageResource(R.drawable.heart_1);
+                heart2.setImageResource(R.drawable.heart_1);
+                heart3.setImageResource(R.drawable.heart_1);
+                sleep(802);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 }
